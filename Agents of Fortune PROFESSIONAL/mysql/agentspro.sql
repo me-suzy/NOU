54482c592e4b994cb944fe5d91689454dab20054
me-suzy <me.suzana@gmail.com>
2025-08-10 13:59:18 +0300

    Add Agents of Fortune PROFESSIONAL/mysql/agentspro.sql

---
+++ Agents of Fortune PROFESSIONAL/mysql/agentspro.sql
@@ -0,0 +1,45 @@
+CREATE TABLE stats (
+	username char(8) DEFAULT 'char' NOT NULL,
+	clicks int,
+	sales int,
+	commission int,
+	sdate char(6) DEFAULT 'char' NOT NULL,
+	mix char(14) DEFAULT 'char' NOT NULL,
+	PRIMARY KEY(mix)
+);
+
+CREATE TABLE byuser (
+	username char(8) DEFAULT 'char' NOT NULL,
+	clicks int,
+	sales int,
+	commission int,
+	period char(2) DEFAULT 'char' NOT NULL,
+	mix char(14) DEFAULT 'char' NOT NULL,
+	PRIMARY KEY(mix)
+);
+
+CREATE TABLE overview (
+	clicks int,
+	sales int,
+	commission int,
+	period char(2) DEFAULT 'char' NOT NULL,
+	PRIMARY KEY(period)
+);
+
+CREATE TABLE bydate (
+	clicks int,
+	sales int,
+	commission int,
+	sdate char(6) DEFAULT 'char' NOT NULL,
+	PRIMARY KEY(sdate)
+);
+
+CREATE TABLE bysale (
+	username char(8) DEFAULT 'char' NOT NULL,
+	commission int,
+	tracer char(40) DEFAULT 'char' NOT NULL,
+	sdate char(6) DEFAULT 'char' NOT NULL,
+	period char(2) DEFAULT 'char' NOT NULL,
+	trackingnumber char(25) DEFAULT 'char' NOT NULL,
+	PRIMARY KEY(trackingnumber)
+);
